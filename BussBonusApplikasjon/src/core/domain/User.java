f662057e0989f90073d170815f8ad28cd9725edf
Olivier-dev-web <olivier.1995@hotmail.com>
2025-11-18 00:43:09 +0100

    Update User.java
    Oppdatert klassen

--- BussBonusApplikasjon/src/core/domain/User.java
+++ BussBonusApplikasjon/src/core/domain/User.java
@@ -7,6 +7,31 @@ public abstract class User {
     String lastName;
     String email;
 
+    //Constructor
+    public User ( int userId, String firstName, String lastName, String email){
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email=email;
+    }
+
+    //getter
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     /*--- Metoder ---
 
     logIn();
@@ -15,11 +40,7 @@ public abstract class User {
     deleteUser();
      */
 
-/*
-* protected int id;
-    protected String firstName;
-    protected String lastName;
-    protected String email;*/
+
 }
 
 
