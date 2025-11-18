0175289f220dc68c22d38310b6f8516fa1a1a04e
Olivier-dev-web <olivier.1995@hotmail.com>
2025-11-18 12:52:04 +0100

    Update Admin.java
    Oppdatert klassen til å extende User klassen og lagt til konstruktør.

--- BussBonusApplikasjon/src/core/domain/Admin.java
+++ BussBonusApplikasjon/src/core/domain/Admin.java
@@ -3,8 +3,9 @@ package core.domain;
 public class Admin extends User{
 
 
+    //Constructor
     public Admin(int userId, String firstName, String lastName, String email) {
-        super(userId, firstName, lastName, email);
+        super(userId, firstName, lastName, email);  //calls the superclass user
     }
 
     /*--- Metoder --- moves to usecases??
