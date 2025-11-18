90bba6f5b27b6868b4e5499c4b714af19734a8fc
Olivier-dev-web <olivier.1995@hotmail.com>
2025-11-18 13:02:24 +0100

    Updated Route
    Oppdatert klassen Route med instansvariabler, konstruktør og getter.

--- BussBonusApplikasjon/src/core/domain/Route.java
+++ BussBonusApplikasjon/src/core/domain/Route.java
@@ -1,11 +1,46 @@
 package core.domain;
 
+import java.time.LocalDateTime;
+
 public class Route {
 
-    int routeID;
-    String startFromBusStop;
-    String stopOnBusStop;
-    //LocalDateTime;
-    boolean routeIsDelayed;
+    private int routeId;
+    private String startFromBusStop;
+    private String stopOnBusStop;
+    private LocalDateTime time;
+    private boolean routeIsDelayed;
 
+
+    //Constructor
+    public Route (int routeId, String startFromBusStop, String stopOnBusStop, LocalDateTime time, boolean routeIsDelayed){
+        this.routeId = routeId;
+        this.startFromBusStop = startFromBusStop;
+        this.stopOnBusStop = stopOnBusStop;
+        this.time = time;
+        this.routeIsDelayed = routeIsDelayed;
+    }
+
+    //Getter
+    public int getRouteId() {
+        return routeId;
+    }
+
+    public String getStartFromBusStop() {
+        return startFromBusStop;
+    }
+
+    public String getStopOnBusStop() {
+        return stopOnBusStop;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public boolean isRouteIsDelayed() {
+        return routeIsDelayed;
+    }
+
+
+    
 }
