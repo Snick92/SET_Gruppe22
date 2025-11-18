289ed3f6187b7b215fa752b2dde244aad388095a
Olivier-dev-web <olivier.1995@hotmail.com>
2025-11-18 12:53:29 +0100

    Update Enduser
    Oppdatert Enduser til å extende User og lagt til konstruktør.

--- BussBonusApplikasjon/src/core/domain/EndUser.java
+++ BussBonusApplikasjon/src/core/domain/EndUser.java
@@ -1,8 +1,13 @@
 package core.domain;
 
-public class EndUser {
+public class EndUser extends User{
 
-/*--- Metoder ---
+    //Constructor
+    public EndUser(int userId, String fistName, String lastName, String email){
+        super(userId, fistName, lastName, email); //calls the superclass user
+    }
+
+/*--- Metoder ---  moves to usecases??
 
 showUserProfile();
 searchRoute();
