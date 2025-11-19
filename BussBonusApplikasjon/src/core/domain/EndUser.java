e9c2664659e71a21e8e7af5be4dc3b2dd305fb16
Olivier-dev-web <olivier.1995@hotmail.com>
2025-11-19 12:19:16 +0100

    Updated Admin & EndUser
    Oppdaterte klassene med  variabelen createdAt.

--- BussBonusApplikasjon/src/core/domain/EndUser.java
+++ BussBonusApplikasjon/src/core/domain/EndUser.java
@@ -1,10 +1,12 @@
 package core.domain;
 
+import java.time.LocalDateTime;
+
 public class EndUser extends User{
 
     //Constructor
-    public EndUser(int userId, String fistName, String lastName, String email){
-        super(userId, fistName, lastName, email); //calls the superclass user
+    public EndUser(int userId, String fistName, String lastName, String email, LocalDateTime createdAt){
+        super(userId, fistName, lastName, email, createdAt); //calls the superclass user
     }
 
 /*--- Metoder ---  moves to usecases??
